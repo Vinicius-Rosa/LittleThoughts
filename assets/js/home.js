ebebe688eddb9963bff714d803dec3936c1db630
Maxwell Ferreira <ferreiramaxwell531@gmail.com>
2019-11-12 09:41:22 -0200

    Implementado a função de adicionar história na home via ajax

---
+++ assets/js/home.js
@@ -0,0 +1,39 @@
+
+function setCKEditorToTextarea() {
+	for(var instanceName in CKEDITOR.instances)
+	CKEDITOR.instances[instanceName].updateElement('.thoughts');            
+}
+
+
+
+$(function(){
+
+	$("#form-thoughts").submit(function(){
+
+		$.ajax({
+			type: "post",
+			url: URL+"home/addHistoryAjax",
+			dataType: "json",
+			data: { 
+				codigo : CKEDITOR.instances.editor1.getData()
+			},
+
+			success: function(data){
+				if(data["error"] == 1){
+					CKEDITOR.instances.editor1.setData( '', function() { this.updateElement(); });
+					$("#teste").html('');
+					for(i = 0; i<data["qtd"]; i++){
+						$("#teste").append("<div class='thoughts'>"+data["historys"][i]["codigo_fonte"]+"</div>");
+					}
+					//window.location.href = URL+"home/index";
+				}else{
+					alert("deu ruim");
+				}
+			}
+		})
+
+		return false;
+	})
+
+
+})
